e32fc32c83254c95f591eae9066571e67974b895
aminul-islam-sajid <mdaminulislamsajid@gmail.com>
2025-07-01 22:53:35 +0600

    setup projuct

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Suspense } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import PublicRouter from './router/publicRouter';
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-     <h1 className='text-7xl font-bold text-red-950 text-center'>Hello</h1>
-    </>
+     <Suspense fallback={''}>
+      <main className="main">
+        <Routes>
+          {PublicRouter.map(({ path, Component }, index) => {
+            return <Route key={index} path={path} element={<Component />} />;
+          })}
+        </Routes>
+      </main>
+    </Suspense>
   )
 }
 
